e32e66db333b4a2f3069695ef2fa20793b0e0861
Simon Dahrs <s.dahrs@gmail.com>
2018-03-14 18:40:57 +0100

    Account creation only possible for users with required nexus role

--- app/controllers/redmine_oauth_controller.rb
+++ app/controllers/redmine_oauth_controller.rb
@@ -64,9 +64,14 @@ class RedmineOauthController < AccountController
         # Profile parse
         info = JSON.parse(response.body)
 
+        roles_req = connection.get do |req|
+          req.url "/nexus/oauth2/roles"
+        end
+
+        roles = JSON.parse(roles_req.body)
         #Login
         if info
-          try_to_login info
+          try_to_login(info, roles)
         else
           flash[:error] = l(:notice_unable_to_obtain_isu_credentials)
           redirect_to signin_path
@@ -76,29 +81,34 @@ class RedmineOauthController < AccountController
   end
 
   # Login
-  def try_to_login info
+  def try_to_login (info, roles)
     params[:back_url] = session[:back_url]
     session.delete(:back_url)
     # Info is NOT provided as { :redmine_login => "ivan", :redmine_attrs => "Ivan|Ivanov|ivan@example.com" }
     user = User.where(:login => info["email"]).first_or_create
     if user.new_record?
-      # Create on the fly
-      user.firstname = info["first_name"]
-      user.lastname = info["last_name"]
-      user.mail = info["email"]
-      user.login = info["email"]
-      user.random_password
-      user.register
-
-      # Here is some really dirty coding, because we override Redmine registration policies
-      user.activate
-      user.last_login_on = Time.now
-      if user.save
-        self.logged_user = user
-        flash[:notice] = l(:notice_account_activated)
-        redirect_to my_account_path
+      # Check if logged in user has te right role(=create_account) to create an account
+      if !roles.include? 'create_account'
+        flash[:error] = 'Geen toegang tot Redmine.'
       else
-        yield if block_given?
+        # Create on the fly
+        user.firstname = info["first_name"]
+        user.lastname = info["last_name"]
+        user.mail = info["email"]
+        user.login = info["email"]
+        user.random_password
+        user.register
+
+        # Here is some really dirty coding, because we override Redmine registration policies
+        user.activate
+        user.last_login_on = Time.now
+        if user.save
+          self.logged_user = user
+          flash[:notice] = l(:notice_account_activated)
+          redirect_to my_account_path
+        else
+          yield if block_given?
+        end
       end
     else
       # Existing record
